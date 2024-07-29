6f2ba08871bea03480c1c54c2dd012911ced3f02
MadhurushaVB <madhurusha2942004@gmail.com>
2024-07-29 21:37:48 +0530

    Initial Commit

--- src/main/com/project/smartofficefacility/config/OfficeConfiguration.java
+++ src/main/com/project/smartofficefacility/config/OfficeConfiguration.java
@@ -1,15 +1,6 @@
 package main.com.project.smartofficefacility.config;
 
-//public class OfficeConfiguration {
-//    public OfficeConfiguration(){
-//        System.out.println("OfficeConfiguration constructor");
-//    }
-//
-//    public void configureRoom(int room, int capacity) {
-//        System.out.println("OfficeConfiguration configureRoom");
-//
-//    }
-//}
+
 
 
 import main.com.project.smartofficefacility.models.Room;
